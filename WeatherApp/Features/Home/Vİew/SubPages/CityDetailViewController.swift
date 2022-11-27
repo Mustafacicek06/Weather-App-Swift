fddbf768ed8f8aecf7eb52204a37288a088ddedd
Mustafa Çiçek <mustafa06cck@gmail.com>
2022-11-27 11:35:41 +0300

    core data starting.

--- WeatherApp/Features/Home/Vİew/SubPages/CityDetailViewController.swift
+++ WeatherApp/Features/Home/Vİew/SubPages/CityDetailViewController.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class CityDetailViewController: UIViewController {
-
+    // MARK: Outlets
+    @IBOutlet private weak var countryLabel: UILabel!
+    @IBOutlet private weak var cityLabel: UILabel!
+    @IBOutlet private weak var celciusLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
+    
+    // MARK: Variables
+    var stringLabel: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getSelectedCityWeather()
         
     }
     
-
+    // MARK: Private Functions
+    private func getSelectedCityWeather() {
+        Client.getWeatherSelectedCity(to: stringLabel ?? "") { [weak self] response, error in
+            if let response = response {
+                self?.countryLabel.text = "Country: \(response.location?.country ?? "")"
+                self?.cityLabel.text = "City: \(response.location?.name ?? "")"
+                self?.celciusLabel.text = "Temperature: \(response.current?.temperature ?? 0)° C"
+                self?.timeLabel.text  = "Time:  \(response.location?.localtime ?? "")"
+            }
+            else {
+                print(error?.localizedDescription)
+            }
+        }
+    }
 }
--- WeatherApp/Managers/Alert/AlertManager.swift
+++ WeatherApp/Managers/Alert/AlertManager.swift
@@ -25,7 +25,7 @@ final class AlertManager: AlertShowable {
         alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
         
         DispatchQueue.main.async {
-            
+            UIApplication.topViewController()?.present(alert, animated: true)
         }
     }
 }
